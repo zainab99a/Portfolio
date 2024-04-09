a69350de1769bca2394b3891712778c9861922c2
zainab99a <amanhal729@gmail.com>
2024-04-09 11:24:47 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function myButton() {
+    let myMenu=document.getElementById('menu');
+    if(myMenu.style.display==='none'){
+        myMenu.style.display='block';
+    }
+    else{
+        myMenu.style.display='none';
+    }
+    
+}
+
